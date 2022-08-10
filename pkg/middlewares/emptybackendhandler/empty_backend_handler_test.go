a51ba49e8fd3e917f986cf2b599223daf628418b
Jay Heinlein <jheinlein@sharecare.com>
2022-08-10 10:54:31 -0400

    Adding in functionality to preserve the index between load balancer recreations

--- pkg/middlewares/emptybackendhandler/empty_backend_handler_test.go
+++ pkg/middlewares/emptybackendhandler/empty_backend_handler_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/traefik/traefik/v2/pkg/testhelpers"
-	"github.com/vulcand/oxy/roundrobin"
+	"github.com/traefik/traefik/v2/pkg/server/service/roundrobin"
 )
 
 func TestEmptyBackendHandler(t *testing.T) {
